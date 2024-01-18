1e8d66bd6d79fca41910fb7b0624f05b10ba5fcd
Timothy Avell <timothy.avell.olatunde@gmail.com>
2024-01-18 06:54:25 -0500

    server side setup

--- server/admin.js
+++ server/admin.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const { appendFile } = require('fs')
 const admin =  express()
-const PORT = process.env.APORT || 4000
+const PORT = process.env.PORT || 4000
+
+admin.get('/',(req,res) => {
+ 
+})
 
 admin.listen(PORT,() => {
  console.log(`Admin server runnong on http://localhost:${PORT}`)
