467acefd6c5b819529b0503f59ee4f147704fa43
Timothy Avell <timothy.avell.olatunde@gmail.com>
2024-01-18 07:00:38 -0500

    server side setup

--- server/admin.js
+++ server/admin.js
@@ -3,8 +3,9 @@ const { appendFile } = require('fs')
 const admin =  express()
 const PORT = process.env.PORT || 4000
 
-admin.get('/',(req,res) => {
- 
+admin.get('/HA/:subpath',(req,res) => {
+ const subpath = req.params.subpath;
+ res.json({'message': `Received POST request for /HV/${subpath}`});
 })
 
 admin.listen(PORT,() => {
--- server/auth.js
+++ server/auth.js
@@ -3,9 +3,10 @@ const express = require('express')
 const auth = express()
 const PORT = process.env.Port ||5000
 
-auth.get('/HV',(req,res) => {
- res.json({'message': 'Auth server'})
-})
+auth.post('/HV/:subpath', (req, res) => {
+ const subpath = req.params.subpath;
+ res.json({'message': `Received POST request for /HV/${subpath}`});
+});
 auth.listen(PORT,() => {
  console.log(`auth server running on http:localhost:${PORT}`)
 })
