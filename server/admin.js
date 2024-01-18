b3ff7100fce3ce36807631e9f26df654f362ef12
Timothy Avell <timothy.avell.olatunde@gmail.com>
2024-01-18 16:59:37 -0500

    server side setup

--- server/admin.js
+++ server/admin.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const path = require('path')
-const { appendFile } = require('fs')
-const admin =  express()
-const PORT = process.env.PORT || 4000
+// const express = require('express')
+// const path = require('path')
+// const { appendFile } = require('fs')
+// const admin =  express()
+// const PORT = process.env.PORT || 4000
 
-admin.get('/HA/:subpath',(req,res) => {
- const subpath = req.params.subpath;
- res.json({'message': `Received POST request for /HV/${subpath}`});
-})
-admin.all('*',(req,res) => {
- const errorFilePath = path.join(__dirname, 'html/error.html')
- res.status(404).sendFile(errorFilePath)
-})
-admin.listen(PORT,() => {
- console.log(`Admin server runnong on http://localhost:${PORT}`)
-})
+// admin.get('/HA/:subpath',(req,res) => {
+//  const subpath = req.params.subpath;
+//  res.json({'message': `Received POST request for /HV/${subpath}`});
+// })
+// admin.all('*',(req,res) => {
+//  const errorFilePath = path.join(__dirname, 'html/error.html')
+//  res.status(404).sendFile(errorFilePath)
+// })
+// admin.listen(PORT,() => {
+//  console.log(`Admin server runnong on http://localhost:${PORT}`)
+// })
