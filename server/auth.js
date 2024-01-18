25b7160bee50511d7f5174f1c5c6eb9fceea033f
Timothy Avell <timothy.avell.olatunde@gmail.com>
2024-01-18 17:03:56 -0500

    404 response setup completed bug fix

--- server/auth.js
+++ server/auth.js
@@ -1,19 +1,19 @@
-// const express = require('express')
-// const path =  require('path')
-// const auth = express()
-// const PORT = process.env.Port ||5000
+const express = require('express')
+const path =  require('path')
+const auth = express()
+const PORT = process.env.Port ||5000
 
-// auth.get('/HV/subpath', (req, res) => {
-//  const subpath = req.params.subpath;
-//  res.json({'message': `Received POST request for /HV/${subpath}`});
-// });
-// auth.all('*',(req,res) => {
-//  const errorFilePath = path.join(__dirname, 'html/error.html')
-//  res.status(404).sendFile(errorFilePath)
-// })
-// auth.listen(PORT,() => {
-//  console.log(`auth server running on http:localhost:${PORT}`)
-// })
+auth.get('/HV/subpath', (req, res) => {
+ const subpath = req.params.subpath;
+ res.json({'message': `Received POST request for /HV/${subpath}`});
+});
+auth.all('*',(req,res) => {
+ const errorFilePath = path.resolve(__dirname, 'html/error.html')
+ res.status(404).sendFile(errorFilePath)
+})
+auth.listen(PORT,() => {
+ console.log(`auth server running on http:localhost:${PORT}`)
+})
 
 // features
 // consumer get restuarant receipes
