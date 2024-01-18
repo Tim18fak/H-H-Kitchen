230cab2a5712dd2c06ce68ccddf4d84970499ed6
Timothy Avell <timothy.avell.olatunde@gmail.com>
2024-01-18 07:02:29 -0500

    server side setup

--- server/auth.js
+++ server/auth.js
@@ -3,7 +3,7 @@ const express = require('express')
 const auth = express()
 const PORT = process.env.Port ||5000
 
-auth.post('/HV/:subpath', (req, res) => {
+auth.get('/HV/:subpath', (req, res) => {
  const subpath = req.params.subpath;
  res.json({'message': `Received POST request for /HV/${subpath}`});
 });
